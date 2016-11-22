1056db1e20eba0ec23ada655cd0cee348f0d478d
Jorn <j.c.rietveld-1@student.tudelft.nl>
2016-11-22 12:16:20 +0100

    Made next level a variable so the portal is multi usable

--- Scripts/Portal.cs
+++ Scripts/Portal.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class Portal : PhilInteractable {
 
+    public string nextLevelName;
+
 	public override void Interact(GameObject interacted)
     {
         Debug.Log("Interacting with base class.");
-        Application.LoadLevel("Tunnel Oval Office");
+        Application.LoadLevel(nextLevelName);
     }
 }
 
