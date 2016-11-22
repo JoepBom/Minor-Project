3e451fd88accf5eae5307d6c15ffc8f1b615b84c
DESKTOP-N045B2Q\Gebruiker <lansvdstephan@gmail.com>
2016-11-22 10:25:30 +0100

    Better walking block

---
+++ Assets/Scripts/PhilMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhilMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private Quaternion Rotation;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void LateUpdate()
+    {
+   
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        // movement
+        Vector3 movement = new Vector3(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+        
+        // turning
+        if (movement == new Vector3(0, 0, 0))
+        {
+            rb.MoveRotation(Rotation);
+        }
+        else
+        {
+            Rotation = Quaternion.LookRotation(movement.normalized);
+            rb.MoveRotation(Rotation);
+        }
+        
+
+    }
+   
+}
