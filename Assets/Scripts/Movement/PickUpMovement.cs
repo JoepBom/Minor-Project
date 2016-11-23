89fcbd0926cfce2535b97867eabdae98c12a3ecd
DESKTOP-N045B2Q\Gebruiker <lansvdstephan@gmail.com>
2016-11-23 13:37:17 +0100

    Simple Health System added 2

---
+++ Assets/Scripts/Movement/PickUpMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpMovement : MonoBehaviour
+{
+    private Vector3 movement;
+
+	// Use this for initialization
+	void Start ()
+    {
+        movement = new Vector3(15, 30, 45);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(movement * Time.deltaTime);
+	}
+}
